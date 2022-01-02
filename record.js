78dce22a9b90d86b9ef6880968a4053ecf948064
Shankara Srikantan <shankarasrikantan@Shankaras-MacBook-Pro.local>
2022-01-02 14:41:55 -0800

    audio working

--- record.js
+++ record.js
@@ -1,14 +1,31 @@
+const audio = document.querySelector('audio');
+const songList = document.querySelector('.songlist');
+
+songList.addEventListener("click", function(e) {
+    title = e.target.closest('li').getAttribute('data-name');
+    artist = e.target.closest('li').getAttribute('data-artist');
+    source = e.target.closest('li').getAttribute('data-src');
+
+    document.querySelector('.title').innerText = title;
+    document.querySelector('.artist-name').innerText  = artist;
+    audio.src = source; 
+
+}, false);
+
 function turnRecord(evt){
     let songName = document.getElementsByClassName('vinyl-print').item(0);
-    
+
     if(songName.style.animationPlayState == "running"){
         songName.style.animationPlayState = "paused";
+        document.getElementById('play-pause').classList = 'fa fa-play';
+        audio.pause();
     }else{
         songName.style.animationPlayState = "running";
+        document.getElementById('play-pause').classList = 'fa fa-pause';
+        audio.play();
     }
 }
-// function turnRecord(evt){
-//     let songName = document.getElementsByClassName('container').item(0);
-    
-//     songName.classList.toggle("play");
-// }
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     toggleButton()
+// });
