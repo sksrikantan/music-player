cd0f18ba7aac54c9b765f1748058600f2bd607fb
Shankara Srikantan <shankarasrikantan@Shankaras-MacBook-Pro.local>
2022-01-03 17:36:56 -0800

    adding new interfaces

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-function openMode(evt, mode){
-    var i, content, links;
-    content = document.getElementsByClassName("mode-content");
-    links = document.getElementsByClassName("mode-nav-buttons");
-    
-    for (i = 0; i < content.length; i++) {
-        content.item(i).style.display = "none";
-    }
-    for (i = 0; i < links.length; i++) {
-        links.item(i).className = links.item(i).className.replace(" active", "");
-    }
-    document.getElementById(mode).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+
+
--- record.js
+++ record.js
@@ -1,7 +1,26 @@
 const audio = document.querySelector('audio');
 const songList = document.querySelector('.songlist');
 const songName = document.querySelector('.vinyl-print');
-const crackle = document.querySelector('#crackle')
+const crackle = document.querySelector('#crackle');
+const hiss = document.querySelector('#hiss');
+sessionStorage.player = 'Vinyl';
+
+function openMode(evt, mode){
+    var i, content, links;
+    content = document.getElementsByClassName("mode-content");
+    links = document.getElementsByClassName("mode-nav-buttons");
+    
+    for (i = 0; i < content.length; i++) {
+        content.item(i).style.display = "none";
+    }
+    for (i = 0; i < links.length; i++) {
+        links.item(i).className = links.item(i).className.replace(" active", "");
+    }
+    sessionStorage.player = mode;
+    switchNoise();
+    document.getElementById(mode).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
 songList.addEventListener("click", function(e) {
 
@@ -16,8 +35,8 @@ songList.addEventListener("click", function(e) {
     
     songName.style.animationPlayState = "running";
     document.getElementById('play-pause').classList = 'fa fa-pause';
-    playVinyl();
-
+    
+    playMusic();
 
 }, false);
 
@@ -26,13 +45,29 @@ function playButton(evt){
     if(songName.style.animationPlayState == "running"){
         songName.style.animationPlayState = "paused";
         document.getElementById('play-pause').classList = 'fa fa-play';
-        pauseVinyl();
+        pauseMusic();
     }
     else{
         songName.style.animationPlayState = "running";
         document.getElementById('play-pause').classList = 'fa fa-pause';
-        playVinyl();
+        playMusic();
     }
+}
+
+function playMusic() {
+    if (sessionStorage.player == 'Vinyl') {
+        playVinyl();
+        } else {
+        playCassette();
+        }
+}
+
+function pauseMusic() {
+    if (sessionStorage.player == 'Vinyl') {
+        pauseVinyl();
+        } else {
+        pauseCassette();
+        }
 }
 
 function playVinyl(){
@@ -43,4 +78,25 @@ function playVinyl(){
 function pauseVinyl(){
     crackle.pause();
     audio.pause();
+}
+function playCassette(){
+    hiss.loop = true;
+    hiss.play();
+    audio.play();
+}
+function pauseCassette(){
+    hiss.pause();
+    audio.pause();
+}
+
+function switchNoise() {
+    crackle.pause();
+    hiss.pause(); 
+    if (songName.style.animationPlayState == "running") {
+        if (sessionStorage.player == 'Vinyl') {
+            crackle.play();
+        } else {
+            hiss.play();
+    }
+    }
 }
