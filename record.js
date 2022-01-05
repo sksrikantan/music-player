085651f06cc3f8e04c69f4a7a5c9426c3b04e191
srikantan-shiama <shiama.sri@gmail.com>
2022-01-04 21:43:19 -0800

    background

--- record.js
+++ record.js
@@ -6,7 +6,7 @@ const hiss = document.querySelector('#hiss');
 const muffle = document.querySelector ('#muffle');
 sessionStorage.player = 'Vinyl';
 const playbtns = document.getElementsByName("play-pause");
-const bgcolors = {"Vinyl": "#F3F6F8", "Cassette": "#9B8E8E", "Radio": "#F3F6F8", "CD": "#F3F6F8"};
+const bgcolors = {"Vinyl": "#F3F6F8", "Cassette": "#F8F4F3", "Radio": "#F8F7F3", "CD": "#F3F6F8"};
 
 function openMode(evt, mode){
     var i, content, links;
