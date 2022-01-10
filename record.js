14ff811fb8fd8955b82bc8928df6e8af74eef6f8
srikantan-shiama <shiama.sri@gmail.com>
2022-01-09 21:59:55 -0800

    radio +cas animation

--- record.js
+++ record.js
@@ -1,6 +1,9 @@
 const audio = document.querySelector('audio');
 const songList = document.querySelector('.songlist');
 const songName = document.querySelector('.vinyl-print');
+// const container = document.getElementsByName("container");
+//have animation such that gear play state is set from here
+const gear = document.querySelectorAll(".gear");
 const crackle = document.querySelector('#crackle');
 const hiss = document.querySelector('#hiss');
 const muffle = document.querySelector ('#muffle');
@@ -41,6 +44,9 @@ songList.addEventListener("click", function(e) {
 
     
     songName.style.animationPlayState = "running";
+    for(var i = 0; i < gear.length; i++){
+        gear.item(i).style.animationPlayState = "running";
+    }
     // document.getElementById('play-pause').classList = 'fa fa-pause';
     
     for(var i = 0; i < playbtns.length; i++){
@@ -55,6 +61,9 @@ function playButton(evt){
 
     if(songName.style.animationPlayState == "running"){
         songName.style.animationPlayState = "paused";
+        for(var i = 0; i < gear.length; i++){
+            gear.item(i).style.animationPlayState = "paused";
+        }
         // document.getElementById('play-pause').classList = 'fa fa-play';
         for(var i = 0; i < playbtns.length; i++){
             playbtns.item(i).classList = 'fa fa-play';
@@ -63,6 +72,9 @@ function playButton(evt){
     }
     else{
         songName.style.animationPlayState = "running";
+        for(var i = 0; i < gear.length; i++){
+            gear.item(i).style.animationPlayState = "running";
+        }
         // document.getElementById('play-pause').classList = 'fa fa-pause';
         for(var i = 0; i < playbtns.length; i++){
             playbtns.item(i).classList = 'fa fa-pause';
