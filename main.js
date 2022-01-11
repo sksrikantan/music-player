86ef03793cb7f57e8cae2d2d38935060d4a40542
Shankara Srikantan <shankarasrikantan@Shankaras-MBP-4.technicolor.net>
2022-01-11 16:32:40 +0100

    retrieves songs from wallet

--- main.js
+++ main.js
@@ -2,20 +2,45 @@ const serverUrl = "https://ofpinptronvn.usemoralis.com:2053/server";
 const appId = "pWdm4Ivq5d8YfPLv6nXYTBa976fo8lZowDQlrJAy";
 Moralis.start({ serverUrl, appId });
 
-async function getList(){
-    const options_rop = { chain: 'ropsten', address: '0xFb0e25CF1eB81b099F5DD9bC32f0D1922D2a80bA' };
+async function getObj(){
+    const options_rop = { chain: 'ropsten', address: localStorage.metamaskAccount};
     const ropNFTs = await Moralis.Web3API.account.getNFTs(options_rop);
     return ropNFTs
 }
 
-async function main(){
-    let NFTList = await getList();
-    console.log(NFTList.result);
-    let first = NFTList.result[0]
-    console.log(first);
-    // isolateAudio(first)
+async function addSong(nft) { 
+    console.log('reached isolate')
+    let ipfs = nft.token_uri.replace("moralis.io:2053", "io")
+    obj = await (await fetch(ipfs)).json();
+
+    var ul = document.querySelector(".songlist");
+    var li = document.createElement("li");
+
+    let audioLink = obj.audio;
+    let audio = audioLink.replace('gateway.pinata.cloud', 'ipfs.io').replace('?preview=1', '');
+    console.log(audio);
+    li.src = audio;
+
+    let imageLink = obj.image;
+    let imageSource = imageLink.replace('gateway.pinata.cloud', 'ipfs.io').replace('?preview=1', '');
+    var image = document.createElement("img");
+    image.src = imageSource;
+    li.appendChild(image);
+
+    var name = document.createElement("span");
+    name.innerHTML = obj.name;
+    li.appendChild(name);
+    
+    ul.appendChild(li);
 }
 
-
+async function main(){
+    let NFTObj = await getObj();
+    const NFTList = NFTObj.result;
+    for (let i = 0; i < NFTList.length; i++) {
+        let nft = NFTList[i]
+        await addSong(nft);
+      }
+}
 
 main();
