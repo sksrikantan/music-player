8ebdcebe204bfa83564ce8a10b6aa6d687e8c710
Shankara Srikantan <shankarasrikantan@Shankaras-MacBook-Pro-4.local>
2022-01-12 20:05:28 +0100

    main functionalities cloes to done

--- library.js
+++ library.js
@@ -3,29 +3,33 @@ const appId = "pWdm4Ivq5d8YfPLv6nXYTBa976fo8lZowDQlrJAy";
 Moralis.start({ serverUrl, appId });
 
 async function getObj(){
-    const options_rop = { chain: 'ropsten', address: localStorage.metamaskAccount};
+    console.log("getObj");
+    const options_rop = { chain: 'eth', address: localStorage.metamaskAccount};
     const ropNFTs = await Moralis.Web3API.account.getNFTs(options_rop);
+    console.log("ropNFTs");
     return ropNFTs
 }
 
 async function addSong(nft) { 
-    console.log('reached isolate')
-    let ipfs = nft.token_uri.replace("moralis.io:2053", "io")
+    console.log("addSong");
+    if (!nft.token_uri) { 
+        return;
+    }
+    let ipfs = nft.token_uri;
     obj = await (await fetch(ipfs)).json();
-
+    console.log(obj);
     var ul = document.querySelector(".songlist");
     var li = document.createElement("li");
 
-    let audioLink = obj.audio;
-    let audio = audioLink.replace('https://gateway.pinata.cloud/ipfs/', 'ipfs://').replace('?preview=1', '');
-    li.src = audio;
-    console.log(li.src);
+    // let audioLink = obj.audio;
+    // let audio = audioLink.replace('https://gateway.pinata.cloud/ipfs/', 'ipfs://').replace('?preview=1', '');
+    // li.src = audio;
+    // console.log(li.src);
 
     let imageLink = obj.image;
     console.log(imageLink);
-    let imageSource = imageLink.replace('https://gateway.pinata.cloud/ipfs/', 'ipfs://').replace('?preview=1', '');
-    var image = document.createElement("img");
-    image.src = imageSource;
+    var image = document.createElement("iframe");
+    image.src = imageLink;
     li.appendChild(image);
 
     var name = document.createElement("span");
@@ -36,18 +40,22 @@ async function addSong(nft) {
 }
 
 async function fetchNFTs(){
+    console.log("fetchNFT");
     let NFTObj = await getObj();
     const NFTList = NFTObj.result;
+    console.log(NFTList);
     for (let i = 0; i < NFTList.length; i++) {
         let nft = NFTList[i]
         await addSong(nft);
       }
 }
 
-function loadLibrary() {
-if (!localStorage.metamaskAccount) { 
-    alert("Connect MetaMask to load NFTs!");
-    } else { 
+async function loadLibrary() {
+    console.log("loadLibrary");
+    if (!localStorage.metamaskAccount) { 
+        alert("Connect MetaMask to load NFTs!");
+    } 
+    else { 
         fetchNFTs();
     }
 }
